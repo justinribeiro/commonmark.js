34da30bd2b1a2e261d6805dea54f51f95709f35a
Justin Ribeiro <justin@justinribeiro.com>
2020-09-16 10:13:06 -0700

    feat: add esm output build

--- rollup.config.js
+++ rollup.config.js
@@ -3,21 +3,27 @@ import nodeResolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import { uglify } from "rollup-plugin-uglify";
+import { terser } from "rollup-plugin-terser";
 
 export default {
     input: "lib/index.js",
     output: [
-      {
-        file: "dist/commonmark.js",
-        format: "umd",
-        name: "commonmark"
-      },
-      {
-        file: "dist/commonmark.min.js",
-        format: "umd",
-        name: "commonmark",
-        plugins: [uglify()]
-      }
+        {
+            file: "dist/commonmark.js",
+            format: "umd",
+            name: "commonmark",
+        },
+        {
+            file: "dist/commonmark.min.js",
+            format: "umd",
+            name: "commonmark",
+            plugins: [uglify()],
+        },
+        {
+            file: "dist/commonmark.esm.js",
+            format: "esm",
+            plugins: [terser()],
+        },
     ],
-    plugins: [nodeResolve(), commonjs(), json()]
+    plugins: [nodeResolve(), commonjs(), json()],
 };
